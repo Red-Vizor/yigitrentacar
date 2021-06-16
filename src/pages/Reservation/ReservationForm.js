05c58c778d1b97301a3e9d191ea1c80abcff2866
iremdgn <iremdgn.1997@gmail.com>
2021-06-16 16:25:41 +0300

    form

--- src/pages/Reservation/ReservationForm.js
+++ src/pages/Reservation/ReservationForm.js
@@ -42,10 +42,108 @@ export default function ReservationForm() {
                         </div>
                     </div>
 
-
-
                 </div>
+
             </div>
+
+            <div className="rezervasyon-uyari text-center p-3">
+                    <p>REZERVASYONUNUZU TAMAMLAMAK İÇİN AŞAĞIDAKİ BİLGİLERİ EKSİKSİZ TAMAMLAYINIZ.</p>    
+            </div>
+
+            <div className="rezervasyon-form">
+            <div className="container mt-5 mb-5 kisisel-bilgiler">
+                <h2  className="text-center mb-5">KİŞİSEL BİLGİLER</h2>
+
+            <form class="row g-3">
+            <div class="col-md-6">
+                <label for="inputEmail4" class="form-label">ADINIZ*</label>
+                <input type="email" class="form-control" id="inputEmail4" placeholder="Adınız (ehliyetinizde yer aldığı gibi)*"/>
+            </div>
+            <div class="col-md-6">
+                <label for="inputPassword4" class="form-label">SOYADINIZ*</label>
+                <input type="password" class="form-control" id="inputPassword4" placeholder="Soyadınız (ehliyetinizde yer aldığı gibi)*"/>
+            </div>
+            <div class="col-md-6">
+                <label for="inputEmail4" class="form-label">CEP TELEFONUNUZ*</label>
+                <input type="email" class="form-control" id="inputEmail4" placeholder="Cep Telefonunuz"/>
+            </div>
+            <div class="col-md-6">
+                <label for="inputPassword4" class="form-label">MAİL ADRESİNİZ*</label>
+                <input type="password" class="form-control" id="inputPassword4" placeholder="Mail Adresiniz"/>
+            </div>
+            <div class="col-md-6">
+                <label for="inputEmail4" class="form-label">DOĞUM TARİHİNİZ*</label>
+                <input type="email" class="form-control" id="inputEmail4" placeholder="GG/AA/YYYY"/>
+            </div>
+            <div class="col-md-6">
+                <label for="inputPassword4" class="form-label">EHLİYET ALIŞ TARİHİNİZ*</label>
+                <input type="password" class="form-control" id="inputPassword4" placeholder="GG/AA/YYYY"/>
+            </div>
+            <div class="col-md-4">
+                <label for="inputState" class="form-label">KİMLİK BİLGİSİ*</label>
+                <select id="inputState" class="form-select">
+                <option selected>TC KİMLİK NO</option>
+                <option>...</option>
+                </select>
+            </div>
+            <div class="col-md-2">
+                <label for="inputZip" class="form-label">TC KİMLİK NO*</label>
+                <input type="text" class="form-control" id="inputZip"/>
+            </div>
+            <div class="col-md-6">
+                <label for="inputState" class="form-label">LOKASYONUNUZ*</label>
+                <select id="inputState" class="form-select">
+                <option selected>İL</option>
+                <option>...</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">MESAJINIZ</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+
+            </form>
+            </div>
+
+            <div className="fatura-bilgileri p-5">
+                <h2 className="text-center mt-3 mb-5">FATURA BİLGİLERİ</h2>  
+
+            <div className="container">
+
+            <form class="row g-3">
+            <div class="col-md-6">
+            <   label for="formGroupExampleInput" class="form-label">ADINIZ*</label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Adınız (ehliyetinizde yer aldığı gibi)*"/>
+            </div>
+            <div class="col-md-6">
+                <label for="formGroupExampleInput" class="form-label">SOYADINIZ*</label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Soyadınız (ehliyetinizde yer aldığı gibi) *"/>
+            </div>
+            <div class="mb-3">
+                <label for="formGroupExampleInput" class="form-label">TC KİMLİK NO</label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder=""/>
+            </div>
+            <div class="mb-3">
+                <label for="formGroupExampleInput2" class="form-label">ADRESİNİZ</label>
+                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="ADRES BİLGİLERİNİZ"/>
+            </div>
+            </form>
+
+            </div>
+            </div>
+
+            </div>
+            
         </section>
+        
+    
     )
+    
 }
+
+
+
+
+    
+
+    
