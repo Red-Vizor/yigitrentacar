b0c0c49b2ad22a43c0086cffe579750225719dc9
iremdgn <iremdgn.1997@gmail.com>
2021-06-18 20:05:38 +0300

    şoförlü araç kiralama sayfası

--- src/pages/Services/DriverServices.js
+++ src/pages/Services/DriverServices.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 export default function DriverServices() {
     return (
-        <div>
-            Driver Services
+        <div className="driver-services">
+            <h4 className="bold text-center">ŞÖFÖRLÜ ARAÇ KİRALAMA & TRANSFER</h4>
+
+            <div className="container corporate-header" style={{ backgroundColor: "#FF710F", height: "90px", width: "75%" }}>
+                <h5 className="bold">İstanbul , İzmir ve Bodrum ofislerimizden 7/24 Kesintisiz kiralama hizmeti
+                almak için bizi arayabilirsiniz.</h5>
+            </div>
+
+            <div class="container driver-services-text mt-4 mb-5" style={{ width: "68%" }}>
+                <p class="bold">Rent a car ya da araç kiralama hizmeti, bireyler için günümüzde önemli bir ihtiyaç haline gelmiştir.</p>
+                <p class="bold">Bireyler, Yiğit oto kiralama' dan ihtiyaçları doğrultusunda ister günlük ister aylık araç kiralayabilmektedir. Günlük ve aylık araç kiralamalarında Yiğit oto kiralamayı tercih ederek, Türkiye’nin lüks ve ekonomik araç kiralamayı bünyesinde bulunduran en geniş araç filosundan yararlanma ayrıcalığına sahip olurlar.</p>
+            </div>
+
+            
+
         </div>
     )
 }
