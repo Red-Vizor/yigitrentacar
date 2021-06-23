0e5f73b36b038fba68f815e8e4f22a9b4c87dac6
iremdgn <iremdgn.1997@gmail.com>
2021-06-23 15:26:44 +0300

    responsive

--- src/pages/Reservation/ReservationForm.js
+++ src/pages/Reservation/ReservationForm.js
@@ -5,7 +5,8 @@ import { PersonalForm, CorporateForm } from '../../Components'
 export default function ReservationForm() {
     return (
         <section className="reservation-form-area">
-            <div className="header-area py-4 bg-pink-powder ">
+            <div className="header-area  py-4 bg-pink-powder ">
+
                 <div className="header-title d-flex mb-3">
                     <span className="title-border-green px-5 py-2 mx-auto bold">
                         REZERVASYONUN ÖZETİ
@@ -13,33 +14,44 @@ export default function ReservationForm() {
                 </div>
                 <div className="header-content d-flex justify-content-center">
                     <div className="row">
-                        <div className="col d-flex">
-                            <img src="./assets/images/s3.png" className="ms-auto w-50" />
+                        <div className="col-lg-4 col-md-4 mx-auto d-flex">
+                            <img src="./assets/images/s3.png"  className="mx-auto reservation-car-image w-50" />
                         </div>
 
-                        <div className="col d-flex">
-                            <div className="car-detail mx-auto my-auto">
+                        <div className="col-lg-5 col-md-5 d-flex">
+                            <div className="car-detail mx-auto my-auto ">
                                 <span className="h4 bold">2021 MERCEDES CLA 180</span>
-                                <div className="row">
-                                    <div className="col-6">
+                                <div className="row ">
+                                    <div className="col-6 bold mt-3 ">
                                         <span>GÜVENCE PAKETİ</span>
                                     </div>
-                                    <div className="col-6">
+                                    <div className="col-6 bold mt-3 ">
                                         <span>EK ÜRÜNLER</span>
                                     </div>
-                                    <div className="col-6">
+                                    <div className="col-6 ">
                                         <span>Mini Hasar Sigortası 1500 TL
-                                            Kapsamlı... Tümünü Göster</span>
+                                            Kapsamlı... <span className="bold">Tümünü Göster</span></span>
                                     </div>
-                                    <div className="col-6">
+                                    <div className="col-6 ">
                                         <span>Mini Hasar Sigortası 1500 TL
-                                            Kapsamlı... Tümünü Göster</span>
+                                            Kapsamlı...  <span className="bold">Tümünü Göster</span></span>
                                     </div>
+                                    
                                 </div>
                             </div>
                         </div>
-                        <div className="col d-flex">
-
+                        <div className="col-lg-3 col-md-3 car-detail d-flex my-auto">
+                            <div className="my-auto ps-lg-5">
+                                <span className="bold h5">REZERVASYON TARİHİ</span>
+                                <div className="mt-lg-3 mt-md-2">
+                                <span >Bodrum, Yalıkavak</span>
+                                </div>
+                                <div>
+                                <span className="bold">21.06.2021 <img src="./assets/icons/arrow-circle.svg" className="my-auto ms-1" /> 21.06.2021</span>
+                                </div>
+                                <span className="bold">340,37₺</span>
+                                
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -52,9 +64,6 @@ export default function ReservationForm() {
             <PersonalForm />
             <CorporateForm />
 
-            <div className="form-policy py-5 container ">
-                Buraya Aydınlatma metni chek gelecek
-            </div>
 
         </section>
 
